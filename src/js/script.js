c656918865d009be02a683d237d050ab40b32ca5
COTO-B <jm.bermudez.s@gmail.com>
2021-07-20 08:17:06 -0400

    initial JS dropdown color

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,29 @@
 // libreria para generar id de hash
-//form nombre proy max 22 caracteres
+
+const colorsContainer = document.querySelector(".color__list");
+
+const colors = document.querySelectorAll(".color__item");
+
+colorsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".color__item");
+
+  console.log(colorsContainer, colors, clicked, "hola", clicked.dataset.color);
+});
+
+//insertar con insertAdjacementHTML afterbegin, en el boton default
+
+// antes de insertar borrar lo que existe
+//  _clear() {
+//     innerHTML = "";
+//   }
+
+const btnShowColor = document.querySelector(".color__btn-dropdown");
+console.log(btnShowColor);
+
+//borrando todo el boton y sus tags
+btnShowColor.innerHTML = "";
+//mostrar el boton seleccionado con el click
+const markup = `<span class="color__item-circle color__item-circle--default"></span>
+<span>TEST color</span>`;
+
+btnShowColor.insertAdjacentHTML("afterbegin", markup);
