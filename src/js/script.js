c2e31ace4c641c0b86a1e6a70b9466a529427ff8
COTO-B <jm.bermudez.s@gmail.com>
2021-07-26 18:57:18 -0400

    save data from form part2

--- src/js/script.js
+++ src/js/script.js
@@ -7,33 +7,17 @@ const btnShowColor = document.querySelector(".color__btn-dropdown");
 const btnColorDropdown = document.querySelector(".color__btn-dropdown");
 const btnNewProjectOpen = document.querySelector(".nav__btn--add-project");
 const btnNewProjectClose = document.querySelector(".btn--close-project-window");
-const btnAddProject = document.querySelector(".upload__btn--add-project");
+// const btnAddProject = document.querySelector(".upload__btn--add-project");
 const overlay = document.querySelector(".overlay");
 const projectWindow = document.querySelector(".add-project-window");
 const form = document.querySelector(".upload");
 const inputProjectName = document.querySelector(".upload__input-name");
-const inputProjectColor = document.querySelector(".color__item-circle");
 const inputProjectDescription = document.querySelector(
   ".upload__input-description"
 );
 
-//FIX:quede aca revisar mapty para guardar en local storage
-//STATE - DATA
-const state = {
-  projectId: "",
-  projectName: "",
-  projectColor: "",
-  projectDescription: "",
-  projectTasks: {
-    id: "",
-    color: "",
-    description: "",
-    date: "",
-  },
-};
 //NEW PROJECT
-//open close window
-
+//open close window function
 const toggleProjectWindow = function () {
   overlay.classList.toggle("hidden");
   projectWindow.classList.toggle("hidden");
@@ -42,8 +26,9 @@ const toggleProjectWindow = function () {
 //open and reset to default
 btnNewProjectOpen.addEventListener("click", function () {
   btnShowColor.innerHTML = "";
-  const defaultMarkup = `<span class="color__item-circle color__item-circle--default"></span>
+  const defaultMarkup = `<span class="color__item-circle color__item-circle--selected color__item-circle--9" data-color="9"></span>
   <span>Electric Blue</span>`;
+
   btnShowColor.insertAdjacentHTML("afterbegin", defaultMarkup);
 
   form.reset();
@@ -63,8 +48,8 @@ btnColorDropdown.addEventListener("click", function (e) {
   //toggle hidden
   colorsContainer.classList.toggle("hidden");
 });
-//FIX:salir cuando hago click afuera del dropdown de los colores
-//REVIEW:ver si añadir seleccion con el teclado
+
+//REVIEW:ver si añadir seleccion con el teclado.salir cuando hago click afuera del dropdown de los colores
 
 //show selected color
 colorsContainer.addEventListener("click", function (e) {
@@ -75,8 +60,9 @@ colorsContainer.addEventListener("click", function (e) {
 
   //borrando todo el boton y sus tags
   btnShowColor.innerHTML = "";
+
   //haciendo el markup
-  const markup = `<span class="color__item-circle color__item-circle--${projectColorNumber}" data-color="${projectColorNumber}"></span>
+  const markup = `<span class="color__item-circle color__item-circle--selected color__item-circle--${projectColorNumber}" data-color="${projectColorNumber}"></span>
   <span>${projectColorName}</span>`;
 
   //mostrar el boton seleccionado con el click
@@ -88,13 +74,15 @@ colorsContainer.addEventListener("click", function (e) {
 
 //FIX:quede aca. crear un array donde esten todos los projects y adentro guardar como objeto(o clase) los proyectos y en el task del project hacer un array para guardar las task como objetos
 
-//ID. cambiar por n alibreria, mientras ocupar 2 random. meter esto cuando hace clik en el btn add project
-
+//ID. cambiar por una alibreria, mientras ocupar 2 random. meter esto cuando hace clik en el btn add project
 const generateProjectId = function () {
   return Math.trunc(Math.random() * 10000000000000);
 };
 
-console.log(generateProjectId());
+//Set Local Storage
+const setLocalStorage = function () {
+  localStorage.setItem("projects", JSON.stringify(projects));
+};
 
 //aca guardo los projectos
 projects = [];
@@ -102,16 +90,33 @@ projects = [];
 //guardo las task que despues empujo al projecto
 tasks = [];
 
-btnAddProject.addEventListener("click", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  //FIX:arreglar dataset siempre tira 9
-  const a = inputProjectName.value;
-  const b = inputProjectColor.dataset.color;
-  const c = inputProjectDescription.value;
+  const inputProjectColor = document.querySelector(
+    ".color__item-circle--selected"
+  );
 
-  console.log(a, b, c);
+  const projectName = inputProjectName.value;
+  const projectColor = inputProjectColor.dataset.color;
+  const projectDescription = inputProjectDescription.value;
+
+  projects.push({
+    projectId: generateProjectId(),
+    projectname: `${projectName}`,
+    projectColor: `${projectColor}`,
+    projectDescription: `${projectDescription}`,
+  });
+  console.log(projects);
+
+  // guardar el local storage. mapty
+  setLocalStorage();
+
+  //FIX: ver como guardar la data. despues dejarlo con clases.. solo esta guardando la ultima en local sotarage revisar. tambien ver stringifly y parse
 
   //cerrar la ventana
   toggleProjectWindow();
 });
+
+const data = JSON.parse(localStorage.getItem("projects"));
+console.log(data);
